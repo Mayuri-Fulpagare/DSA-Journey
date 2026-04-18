f2f44834816bafca8c063acca2fb5a2615407de1
Mayuri0x <mayurifulpagare13@gmail.com>
2026-04-18 22:28:54 +0530

    Time: 7 ms (74.36%) | Memory: 49.9 MB (38.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,32 +1,17 @@
-
+import java.util.*;
 
 class Solution {
-    public String getSignature(String s) {
-        int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                sb.append((char) ('a' + i)).append(count[i]);
-            }
-        }
-        return sb.toString();
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        Map<String, List<String>> groups = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            groups.computeIfAbsent(getSignature(s), k -> new ArrayList<>()).add(s);
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
-        result.addAll(groups.values());
-
-        return result;
+        return new ArrayList<>(map.values());
     }
 }
-
