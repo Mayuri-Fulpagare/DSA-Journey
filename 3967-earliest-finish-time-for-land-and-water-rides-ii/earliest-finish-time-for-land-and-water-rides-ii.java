1424eb5fb9ff22f5ca0445123d0adaa0ad8eec86
Mayuri0x <mayurifulpagare13@gmail.com>
2026-06-03 21:48:56 +0530

    Time: 79 ms (24.14%) | Memory: 86.8 MB (98.28%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,69 +1,71 @@
-// class Solution {
-//     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        
-//     }
-// }
-
 import java.util.Arrays;
 
 class Solution {
-    int minFinish = Integer.MAX_VALUE;
+    int minFinish;
 
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
         int n = landStartTime.length;
         int m = waterStartTime.length;
         
-        // Group start times and durations
-        int[][] land = new int[n][2];
+        // Pack start time and duration into a single 64-bit primitive long
+        long[] land = new long[n];
         for (int i = 0; i < n; i++) {
-            land[i][0] = landStartTime[i];
-            land[i][1] = landDuration[i];
+            land[i] = ((long) landStartTime[i] << 32) | landDuration[i];
         }
         
-        int[][] water = new int[m][2];
+        long[] water = new long[m];
         for (int i = 0; i < m; i++) {
-            water[i][0] = waterStartTime[i];
-            water[i][1] = waterDuration[i];
+            water[i] = ((long) waterStartTime[i] << 32) | waterDuration[i];
         }
         
-        // Sort both by start times
-        Arrays.sort(land, (a, b) -> Integer.compare(a[0], b[0]));
-        Arrays.sort(water, (a, b) -> Integer.compare(a[0], b[0]));
+        // Sorting primitive arrays is incredibly fast compared to object arrays
+        Arrays.sort(land);
+        Arrays.sort(water);
         
         minFinish = Integer.MAX_VALUE;
         
-        // Try both permutations: land then water, and water then land
-        checkOrder(land, water);
-        checkOrder(water, land);
+        // Pre-allocate to prevent multiple memory allocations in our helper method
+        int maxLen = Math.max(n, m);
+        int[] preMinDuration = new int[maxLen];
+        int[] sufMinFinish = new int[maxLen];
+        
+        checkOrder(land, water, preMinDuration, sufMinFinish);
+        checkOrder(water, land, preMinDuration, sufMinFinish);
         
         return minFinish;
     }
     
-    private void checkOrder(int[][] first, int[][] second) {
+    private void checkOrder(long[] first, long[] second, int[] preMinDuration, int[] sufMinFinish) {
         int m = second.length;
-        int[] preMinDuration = new int[m];
-        int[] sufMinFinish = new int[m];
         
-        // Prefix min for durations (if ride is already open)
-        preMinDuration[0] = second[0][1];
+        // (int) second[i] extracts the bottom 32 bits (duration)
+        // (int) (second[i] >>> 32) extracts the top 32 bits (startTime)
+        
+        preMinDuration[0] = (int) second[0];
         for (int i = 1; i < m; i++) {
-            preMinDuration[i] = Math.min(preMinDuration[i-1], second[i][1]);
+            int dur = (int) second[i];
+            preMinDuration[i] = preMinDuration[i-1] < dur ? preMinDuration[i-1] : dur;
         }
         
-        // Suffix min for finish times (if we have to wait for the ride to open)
-        sufMinFinish[m-1] = second[m-1][0] + second[m-1][1];
+        int lastStart = (int) (second[m-1] >>> 32);
+        int lastDur = (int) second[m-1];
+        sufMinFinish[m-1] = lastStart + lastDur;
+        
         for (int i = m - 2; i >= 0; i--) {
-            sufMinFinish[i] = Math.min(sufMinFinish[i+1], second[i][0] + second[i][1]);
+            int start = (int) (second[i] >>> 32);
+            int dur = (int) second[i];
+            int finish = start + dur;
+            sufMinFinish[i] = sufMinFinish[i+1] < finish ? sufMinFinish[i+1] : finish;
         }
         
         for (int i = 0; i < first.length; i++) {
-            int finishFirst = first[i][0] + first[i][1];
+            int finishFirst = (int) (first[i] >>> 32) + (int) first[i];
             
-            // Custom binary search (upper bound) to find the split point
+            // Binary search 
             int left = 0, right = m;
             while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (second[mid][0] <= finishFirst) {
+                int mid = (left + right) >>> 1;
+                if ((int) (second[mid] >>> 32) <= finishFirst) {
                     left = mid + 1;
                 } else {
                     right = mid;
@@ -72,14 +74,13 @@ class Solution {
             
             int pos = left - 1;
             
-            // Option 1: We take a second ride that is already open when we finish the first
             if (pos >= 0) {
-                minFinish = Math.min(minFinish, finishFirst + preMinDuration[pos]);
+                int cand1 = finishFirst + preMinDuration[pos];
+                if (cand1 < minFinish) minFinish = cand1;
             }
-            
-            // Option 2: We wait for a second ride that opens after we finish the first
             if (pos + 1 < m) {
-                minFinish = Math.min(minFinish, sufMinFinish[pos + 1]);
+                int cand2 = sufMinFinish[pos + 1];
+                if (cand2 < minFinish) minFinish = cand2;
             }
         }
     }
